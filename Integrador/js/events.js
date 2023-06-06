5c4b6bc7b58665f2a5e3ab9aa8161807711dea1e
EZR4N <124017711+EZR4N@users.noreply.github.com>
2023-06-05 22:26:30 -0300

    Update events.js

--- Integrador/js/events.js
+++ Integrador/js/events.js
@@ -1,7 +1,7 @@
 const buttons =document.getElementsByClassName('comprarTickets')
 
 const btnClick= (e)=>{
-    location.href='integrador2/index.html'
+    location.href='/integrador2/index.html'
 }
 
 buttons[0].addEventListener('click', btnClick, false)
